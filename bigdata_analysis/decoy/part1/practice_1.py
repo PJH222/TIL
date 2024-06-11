fdf711616d75b176e014dae92d31610041b5f4da
PJH <riminiop@naver.com>
2024-06-11 21:08:43 +0900

    0611

--- bigdata_analysis/decoy/part1/practice_1.py
+++ bigdata_analysis/decoy/part1/practice_1.py
@@ -1,15 +1,19 @@
+# Cars93데이터셋의 Wheelbase 컬럼에 대해 평균값에서 표준편
+
 import pandas as pd
 
-exam1 = pd.read_csv("decoy/practice/Cars93.csv")
+exam1 = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Cars93.csv")
 # exam1 = pd.read_csv('data/연습문제/Cars93.csv')
 
-print(exam1)
+# print(exam1)
 
 # std 1.5배
 wheelbase = exam1['Wheelbase']
 # print(wheelbase)
 wheelbase_mean = wheelbase.mean()
-print(wheelbase_mean)
+wheelbase_std = wheelbase.std()
+
+
 
 
 
