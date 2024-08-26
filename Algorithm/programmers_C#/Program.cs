a7793d922ec3ae27e9fd953f9df38fb66be90a54
PJH <riminiop@naver.com>
2024-08-26 21:52:20 +0900

    0826

---
+++ Algorithm/programmers_C#/Program.cs
@@ -0,0 +1,12 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {   
+        Console.WriteLine("왕왕바보");
+    }
+}
