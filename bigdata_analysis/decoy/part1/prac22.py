48b2013886e62963295e1a7fbc347d0d2e891f2b
PJH <riminiop@naver.com>
2024-11-19 22:46:12 +0900

    1119 Cheer up :<

--- bigdata_analysis/decoy/part1/prac22.py
+++ bigdata_analysis/decoy/part1/prac22.py
@@ -114,20 +114,294 @@ ex = pd.read_csv('C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Cars93.csv')
 
 # print(ex[cond1 | cond2 ].shape[0])
 
-mak = ex['Make'].copy()
+# mak = ex['Make'].copy()
 
-# print(mak)
-mak2 = mak.copy().str.strip()
-cond1 = mak.str.startswith(('Chevrolet','Pontiac','Hyundai'))
-cond11 = mak2.str.startswith(('Chevrolet','Pontiac','Hyundai'))
-print(mak[cond1].shape)
-print(mak2[cond1].shape)
-air = ex['AirBags']
-cond2 = (air == "Driver only")
-# print(air[cond2])
-answer = ex[cond1 & cond2].shape
-answer2 = ex[cond11 & cond2].shape
+# # print(mak)
+# mak2 = mak.copy().str.strip()
+# cond1 = mak.str.startswith(('Chevrolet','Pontiac','Hyundai'))
+# cond11 = mak2.str.startswith(('Chevrolet','Pontiac','Hyundai'))
+# print(mak[cond1].shape)
+# print(mak2[cond1].shape)
+# air = ex['AirBags']
+# cond2 = (air == "Driver only")
+# # print(air[cond2])
+# answer = ex[cond1 & cond2].shape
+# answer2 = ex[cond11 & cond2].shape
 
-print(mak[cond1], mak2[cond11])
-print(mak[0:20])
-# print(mak, mak11)
+# print(mak[cond1], mak2[cond11])
+# print(mak[0:20])
+# # print(mak, mak11)
+
+# mak = ex['Make']
+# df1 = mak.copy()
+# df1 = df1.str.strip()
+# cond1 = df1.str.startswith(('Chevrolet','Pontiac','Hyundai'))
+
+# air = ex['AirBags']
+# # print(air.value_counts())
+# cond2 = (air == 'Driver only')
+
+# answer = ex[cond1 & cond2].shape
+# print(answer[0])
+
+# ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Rabbit.csv")
+
+# dos = ex['Dose']
+# tmp1 = dos.quantile(0.75)
+# tmp2 = dos.quantile(0.5)
+# answer = abs(tmp1 - tmp2)
+# print(int(answer))
+
+
+# ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Boston.csv")
+# # print(max(ex['medv']))
+
+# ex['medv_cut'] = pd.cut(ex['medv'],bins = [0,10,20,30,40,50,60])
+# # print(medv_cut.max())
+
+# mode = ex['medv_cut'].value_counts().idxmax()
+# cond = (ex['medv_cut'] == mode)
+
+# print(round(ex['dis'][cond].median(),2))
+
+ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Melanoma.csv")
+# df1 = ex[:123]
+# df2 = ex[123:]
+
+# thi1 = df1['thickness']
+# thi2 = df2['thickness']
+
+# men1 = thi1.mean()
+# men2 = thi2.mean()
+
+# std1 = thi1.std()
+# std2 = thi2.std()
+
+# aa = (thi1 - men1)/std1
+# bb = (thi2 - men2)/std2
+
+# result1 = aa[(aa >= -1) & (aa <= 1)].median()
+# result2 = bb[(bb >= -1) & (bb <= 1)].median()
+
+# answer = round(result1 + result2, 4)
+# print(answer)
+
+# df1 = ex.copy().iloc[:123]
+# df2 = ex.copy().iloc[123:]
+
+# men1 = df1['thickness'].mean()
+# men2 = df2['thickness'].mean()
+
+# sd1 = df1['thickness'].std()
+# sd2 = df2['thickness'].std()
+
+# aa1 = (df1['thickness'] - men1) / sd1
+# aa2 = (df2['thickness'] - men2) / sd2
+
+# res1 = aa1[(aa1>=-1)&(aa1<=1)].median()
+# res2 = aa2[(aa2>=-1)&(aa2<=1)].median()
+# print(round(res1+res2,4))
+
+# df1 = ex.iloc[:123]
+# df2 = ex.iloc[123:]
+
+# avg = df1['thickness'].mean()
+# sd = df1['thickness'].std()
+
+# std1 = (df1['thickness'] - avg) / sd
+# std2 = (df2['thickness'] - avg) / sd
+
+# res1 = std1[(std1 > -1)&(std1 < 1)].median()
+# res2 = std2[(std2 > -1)&(std2 < 1)].median()
+
+# answer = round(res1+res2 , 4)
+# print(answer)
+
+ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Cars93.csv")
+# ex['rank'] = ex['Length'].rank(method = "average")
+# rank = ex['rank']
+
+# a = ex['Length'][rank < 30].std()
+# answer = round(a,3)
+# print(answer)
+
+# maxx = ex['Max_Price'].sort_values(ascending=False, ignore_index = True)
+# minn = ex['Min_Price'].sort_values(ascending=True, ignore_index = True)
+
+# answer = round((maxx - minn).std(),3)
+# print(answer)
+# wei = ex['Weight']
+# men = wei.mean()
+# stdd = wei.std()
+
+# wei = (wei - wei.min()) / (wei.max() - wei.min())
+# aa = wei[wei < 0.5].var()
+# bb = wei[wei > 0.5].var()
+# answer = round(abs(aa-bb),3)
+# print(answer)
+
+# aa = ex.drop_duplicates(('Manufacturer','Origin')).shape[0]
+# ex['22'] = ex['Manufacturer'].str[:2]
+# bb = ex.drop_duplicates(('22','Origin')).shape[0]
+
+# answer = abs(aa - bb)
+# print(answer)
+
+# cnt = ex.groupby(['Type','Man_trans_avail'])['RPM'].count()
+# summ = ex.groupby(['Type','Man_trans_avail'])['RPM'].sum()
+# medd = ex.groupby(['Type','Man_trans_avail'])['RPM'].median()
+
+# # print(cnt,summ,medd)
+
+# res = sum(medd - summ / cnt)
+# answer = res
+# print(round(answer,0))
+
+# menn = ex['RPM'].mean()
+# ex['RPM'] = ex['RPM'].fillna(menn)
+
+# r = ex['RPM']
+# mm = r.mean()
+# sd = r.std()
+
+# a = (r - mm) / sd
+
+# w = ex['Wheelbase']
+# m = w.mean()
+# sdd = w.std()
+
+# b = (w - m) / sdd
+
+# result = b * -36 - a
+# answer = round(result.std(), 3)
+# print(answer)
+
+# ex1 = ex.copy()
+# ex2 = ex.copy()
+
+
+# pri = ex['Price'].copy()
+# men = pri.mean()
+# ex1['Price'] = pri.fillna(men)
+# pri = ex1['Price']
+
+
+# men = (ex['Max_Price'] + ex['Min_Price']) / 2
+
+# tmp1 = ex[pri < men]
+# # print(tmp1)
+# aa = tmp1.groupby(['Origin'])['Price'].sum()
+# print(aa)
+
+
+
+# pri = ex2['Price']
+# med = pri.median()
+# ex2['Price'] = ex2['Price'].fillna(med)
+
+# q3 = ex2['Min_Price'].quantile(0.75)
+# cond = (pri < q3)
+
+# ex2 = ex[cond]
+# tmp2 = ex2.groupby(['Origin'])['Price'].sum()
+# print(tmp2)
+
+# print(((aa + tmp2).max()))
+
+# ex1 = ex.copy()
+# ex2 = ex.copy()
+
+# menn = ex1['Price'].mean()
+# ex1['Price'] = ex1['Price'].fillna(menn)
+# ex1['menn'] = (ex['Max_Price'] + ex['Min_Price']) / 2
+# mennn = ex1['menn']
+# pri = ex1['Price']
+# cond1 = (pri < mennn)
+# ex1 = ex1[cond1]
+
+# result1 = ex1.groupby(['Origin'])['Price'].sum()
+
+# med = ex2['Price'].median()
+# ex2['Price'] = ex2['Price'].fillna(med)
+
+# q3 = ex2['Min_Price'].quantile(0.75)
+# pri2 = ex2['Price']
+# cond2 = (pri2 < q3)
+# ex2 = ex2[cond2]
+
+# result2 = ex2.groupby(['Origin'])['Price'].sum()
+
+# answer = max(result1 + result2)
+# print(int(answer))
+
+# ex['menn'] = (ex['Max_Price'] + ex['Min_Price']) / 2
+# cond = ex['Price'].isna()
+
+# pri = ex['Price']
+# pri[cond] = ex['menn']
+
+# cond1 = (pri < 14.7)
+
+# # print(ex.columns)
+# ty = ex['Type']
+# cond2 = ((pri > 25.3) & (ty == "Large"))
+# answer = ex[cond1 | cond2].shape[0]
+# print(answer)
+
+
+# mak = ex['Make']
+# mak = mak.str.strip()
+# cond1 = mak.str.startswith(('Chevrolet','Pontiac','Hyundai'))
+
+# ai = ex['AirBags']
+# cond2 = (ai == 'Driver only')
+
+# answer = ex[cond1 & cond2].shape[0]
+# print(answer)
+
+# ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Rabbit.csv")
+
+# do = ex['Dose']
+# q2 = do.quantile(0.5)
+# q3 = do.quantile(0.75)
+
+# answer = int(abs(q2-q3))
+# print(answer)
+
+# ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Boston.csv")
+
+# # print(ex['medv'].max())
+# medv_bin = pd.cut(ex['medv'],bins = [0,10,20,30,40,50,60])
+# cnt = medv_bin.value_counts().idxmax()
+
+# cond = (medv_bin == cnt)
+# res = ex['dis'][cond].median()
+# answer = round(res,2)
+# print(answer)
+
+ex = pd.read_csv("C:/Users/asd/TIL/bigdata_analysis/decoy/practice/Melanoma.csv")
+df1 = ex.copy()[:123]
+df2 = ex.copy()[123:]
+
+men = df1['thickness'].mean()
+sd = df1['thickness'].std()
+
+print(men, sd)
+dt1 = df1['thickness']
+dt2 = df2['thickness']
+
+df1['zz'] = (dt1 - men) / sd
+df2['zz'] = (dt2 - men) / sd
+
+
+zz1 = df1['zz'].copy()
+zz2 = df2['zz'].copy()
+# print(zz1)
+cond1 = ((zz1 < 1) & (zz1 > -1))
+cond2 = ((zz2 < 1) & (zz2 > -1))
+
+res1 = zz1[cond1].median()
+res2 = zz2[cond2].median()
+
+answer = round(res1 + res2 , 4)
+print(answer)
